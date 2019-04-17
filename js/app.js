ab5690b6408f866bf059f853cfcfd8dcfe8c6870
Benk0033 <benk0033@gmail.com>
2019-04-16 23:34:26 -0700

    search returns 10 headlines now, start and end dates work now, and clear button works

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // var searchTerm = "NASA";
-var startYear = 20180101;
-var endYear = 20181231;
+// var startYear = 20180101;
+// var endYear = 20181231;
 
 
 $("#find-article").on("click", function (event) {
@@ -8,6 +8,9 @@ $("#find-article").on("click", function (event) {
     event.preventDefault();
 
     var searchTerm = $("#article-input").val();
+    var startYear = $("#start-input").val();
+    var endYear = $("#end-input").val();
+
 
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&begin_date=" + startYear + "&end_date=" + endYear + "&api-key=3SIwRHd1BsdHSaTiAaYDzg8tDHpWVHw9";
 
@@ -22,10 +25,17 @@ $("#find-article").on("click", function (event) {
         //response as object
         console.log(response);
 
-        $("#article-view").text(JSON.stringify(results.docs[1].byline))
-
+        for (i = 0; i < 10; i++) {
+            var articleDiv = $("<div class='article'>");
+            var articleDisplay = articleDiv.text(JSON.stringify(results.docs[i].headline.main))
+            $("#article-view").append(articleDisplay);
+        };
         //response as string
         // console.log(JSON.stringify(response));
     });
 
+});
+
+$("#clear").on("click", function () {
+    $("#article-view").empty();
 });
